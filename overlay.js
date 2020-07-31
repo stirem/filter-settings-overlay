59fc1ae2ecda05096dda9db99f0d614c1603fd28
Stian Remvik <stirem@gmail.com>
2020-07-31 14:09:15 +0200

    Buttons with styling

--- overlay.js
+++ overlay.js
@@ -58,22 +58,36 @@ var overlay = (function() {
 
 
   // TAGS BUTTONS
-  var tags = ['Arrangement', 'Future Divercities', 'Generelt', 'Kunngjøring', 'Utvikling', 'Verktøykassen'];
+  var tags = ['Arrangement', 'Future Divercities', 'Generelt', 'Kunngjøring', 'Utvikling', 'Verktøykassen', '#dele', '#eksperimentere', '#skape', '3d', 'algoritmer', 'animasjon', 'app', 'arbeidsresidency', 'BEK Studio Sessions', 'co-creation', 'DIY', 'feltopptak', 'fiberoptikk', 'film', 'gjestekunstner', 'hardware-utvikling', 'installasjon', 'interaktiv', 'komposisjon', 'konsert', 'kunstnerisk utviklingsarbeid', 'lyd', 'maskinvare', 'meetup', 'musikk', 'omsluttende lyd', 'performance', 'presentasjon', 'Programvare', 'programvareutvikling', 'publikasjonsutvikling', 'rad', 'scenekunst', 'seminar', 'Spatialisering', 'spill', 'stedsspesifikk', 'tekst', 'utstilling', 'verktøykassen', 'video', 'Virtual Reality', 'vitenskapelig', 'forskning', 'workshop'];
   var buttons;
+  var buttonIsSelected = [];
 
   function makeButtons() {
-      for ( var i = 0; i < tags.length; i++ ) {
-         var btn = document.createElement( "button" );
-         btn.setAttribute( "onclick", "overlay.buttonClick( this.id )" );
-         btn.setAttribute( "id", i );
-         var t = document.createTextNode( tags[i] );
-         btn.appendChild( t );
-         document.querySelector( '.tags' ).appendChild( btn );
-      }
+    for ( var i = 0; i < tags.length; i++ ) {
+      var btn = document.createElement( "button" );
+      btn.setAttribute( "onclick", "overlay.buttonClick( this.id )" );
+      btn.setAttribute( "id", i );
+      btn.setAttribute( "class", "tags-buttons" )
+      var t = document.createTextNode( tags[i] );
+      btn.appendChild( t );
+      document.querySelector( '.tags' ).appendChild( btn );
+    }
   }
 
   function buttonClick( i ) {
     console.log( "button clicked: ", i );
+    if ( ! buttonIsSelected[i] ) {
+      document.getElementById( i ).style.background = "#ff0000";
+      document.getElementById( i ).style.color = "#fff";
+      document.getElementById( i ).style.transform = "translateY( 4px )";
+      buttonIsSelected[i] = true;
+    } else {
+      document.getElementById( i ).style.background = "#9D9D9D";
+      document.getElementById( i ).style.color = "#000";
+      document.getElementById( i ).style.transform = "translateY( 0px )";
+      buttonIsSelected[i] = false;
+    }
+
   }
 
 
