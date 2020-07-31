4bf0db56abe99671ba7949903ebc321fe497d5c4
Stian Remvik <stirem@gmail.com>
2020-07-31 11:34:41 +0200

    Buttons working

--- overlay.js
+++ overlay.js
@@ -1,7 +1,6 @@
 var overlay = (function() {
   'use strict';
 
-  var isShowing = false;
 
   // INIT
   window.onload = function() {
@@ -9,8 +8,9 @@ var overlay = (function() {
     makeButtons();
   }
 
-
   // OVERLAY BOX
+  var isShowing = false;
+
   function show() {
     if ( ! isShowing ) {
       document.querySelector( '.overlay-box' ).style.display = "block";
@@ -24,13 +24,13 @@ var overlay = (function() {
   // SLIDERS
   function makeSliders() {
     var sliderSections = document.getElementsByClassName( "slider" );
-    for ( var x = 0; x < sliderSections.length; x++ ) {
-      var sliders = sliderSections[x].getElementsByTagName( "input" );
-      for ( var y = 0; y < sliders.length; y++ ) {
-        if ( sliders[y].type === "range" ) {
-          sliders[y].oninput = getSliderValues;
+    for ( var i = 0; i < sliderSections.length; i++ ) {
+      var sliders = sliderSections[i].getElementsByTagName( "input" );
+      for ( var j = 0; j < sliders.length; j++ ) {
+        if ( sliders[j].type === "range" ) {
+          sliders[j].oninput = getSliderValues;
           // Manually trigger event first time to display values
-          sliders[y].oninput();
+          sliders[j].oninput();
         }
       }
     }
@@ -58,23 +58,22 @@ var overlay = (function() {
 
 
   // TAGS BUTTONS
-  var tags = [ 'Arrangement', 'Future Divercities', 'Generelt Kunngjøring', 'Utvikling', 'Verktøykassen'];
+  var tags = ['Arrangement', 'Future Divercities', 'Generelt', 'Kunngjøring', 'Utvikling', 'Verktøykassen'];
+  var buttons;
 
   function makeButtons() {
       for ( var i = 0; i < tags.length; i++ ) {
          var btn = document.createElement( "button" );
+         btn.setAttribute( "onclick", "overlay.buttonClick( this.id )" );
+         btn.setAttribute( "id", i );
          var t = document.createTextNode( tags[i] );
          btn.appendChild( t );
          document.querySelector( '.tags' ).appendChild( btn );
       }
   }
 
-  var buttons = document.getElementsByTagName( "button" );
-  for ( var i = 0; i < buttons.length; i++ ) {
-    buttons[i].onclick = function() {
-      console.log( "click" );
-      buttons[i].style.background='#000000';
-    }
+  function buttonClick( i ) {
+    console.log( "button clicked: ", i );
   }
 
 
@@ -82,7 +81,7 @@ var overlay = (function() {
   // RETURN
   return {
     show: show,
-    getSliderValues: getSliderValues
+    buttonClick: buttonClick
   }
 
 })();
