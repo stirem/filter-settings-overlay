60bb8918116c8c13eafe0fb8514131e8d296103f
stirem <stirem@gmail.com>
2020-08-21 14:02:27 +0200

    Working red line

--- overlay.js
+++ overlay.js
@@ -1,16 +1,11 @@
 var overlay = (function() {
   'use strict';
 
-  // SLIDER
-  addEventListener('input', e => {
-    let _t = e.target;
-    _t.parentNode.style.setProperty(`--${_t.id}`, +_t.value);
-  }, false);
-
 
   // INIT
   window.onload = function() {
     makeButtons();
+    makeSliders();
   }
 
   // OVERLAY BOX
@@ -20,12 +15,60 @@ var overlay = (function() {
     if ( ! isShowing ) {
       document.querySelector( '.overlay-box' ).style.display = "block";
       isShowing = true;
+      setSliderWhiteLinePos();
     } else {
       document.querySelector( '.overlay-box' ).style.display = "none";
       isShowing = false;
     }
   }
 
+
+  // SLIDER
+  addEventListener('input', e => {
+    let _t = e.target;
+    _t.parentNode.style.setProperty(`--${_t.id}`, +_t.value);
+  }, false);
+
+  function makeSliders() {
+    var sliders = document.getElementsByTagName( "input" );
+      for ( var j = 0; j < sliders.length; j++ ) {
+        if ( sliders[j].type === "range" ) {
+        sliders[j].oninput = getSliderValues;
+        // Manually trigger event first time to display values
+        sliders[j].oninput();
+      }
+    }
+  }
+
+  function getSliderValues() {
+    // Get slider values
+    var slides = document.getElementsByTagName( "input" );
+    var slide1 = parseFloat( slides[0].value );
+    var slide2 = parseFloat( slides[1].value );
+    // Neither slider will clip the other, so make sure we determine which is larger
+    if ( slide1 > slide2 ) {
+      var tmp = slide2;
+      slide2 = slide1;
+      slide1 = tmp;
+    }
+
+    var sliderValueFrom = document.querySelector( ".slider-value-from" );
+    sliderValueFrom.innerHTML = "Fra: " + slide1;
+    var sliderValueTo = document.querySelector( ".slider-value-to" );
+    sliderValueTo.innerHTML = "Til: " + slide2;
+
+  }
+
+  function setSliderWhiteLinePos() {
+    var slides = document.getElementsByTagName( "input" );
+    var slideRect = slides[0].getBoundingClientRect();
+    var whiteLine = document.querySelector( ".white-line" );
+    whiteLine.style.top = slideRect.top + (slideRect.height/2) - 1 + "px";
+    var wrapRect = document.querySelector( ".wrap" ).getBoundingClientRect();
+    whiteLine.style.width = wrapRect.width + "px";
+    var overlayRect = document.querySelector( ".overlay-box" ).getBoundingClientRect();
+    whiteLine.style.left = wrapRect.left - overlayRect.left + "px";
+  }
 
 
   // TAGS BUTTONS
